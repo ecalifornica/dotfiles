39d1bb64ccfd0e053bd7d8e9c92a343db4af67fc
Robert Queenin <queenin@gmail.com>
2017-03-26 01:45:39 +0000

    removing files before symlinking

--- welcome.sh
+++ welcome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s dotfiles/.tmux.conf ~/.tmux.conf
-ln -s dotfiles/.vimrc ~/.vimrc
-ln -s dotfiles/.gitconfig ~/.gitconfig
-ln -s dotfiles/.bash_aliases ~/.bash_aliases
+rm -f ~/.tmux.conf && ln -s dotfiles/.tmux.conf ~/.tmux.conf
+rm -f ~/.vimrc && ln -s dotfiles/.vimrc ~/.vimrc
+rm -f ~/.gitconfig && ln -s dotfiles/.gitconfig ~/.gitconfig
+rm -f ~/.bash_aliases && ln -s dotfiles/.bash_aliases ~/.bash_aliases
